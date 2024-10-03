71485b4588bed0d1b72b77942cbca9e6ebf85e2b
adhithiravi <adhithiravi@gmail.com>
2024-10-03 15:21:07 -0500

    Data fetching in all shop pages

--- app/shop/cheesecakes/page.tsx
+++ app/shop/cheesecakes/page.tsx
@@ -1,7 +1,47 @@
-export default function Cheesecake() {
+import Image from "next/image";
+import AddToCartButton from "@/ui/components/Button/AddToCartButton";
+
+// Dynamic data fetching, no cache
+export default async function Cheesecakes() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/pies?category=cheesecakes`,
+    { cache: "no-store" }
+  );
+  const pies = await res.json();
+
   return (
     <>
-      <h2>Shop for Cheese Cakes</h2>
+      <section className="main-content">
+        <h2>
+          Cheesecakes
+          <hr />
+        </h2>
+
+        <div className="gallery-wrapper">
+          {pies.map((pie: any) => (
+            <>
+              <div key={pie.id} className="pie-item">
+                <Image
+                  src={pie.imageUrl}
+                  alt={pie.name}
+                  width={400}
+                  height={200}
+                />
+                <AddToCartButton
+                  id={pie.id}
+                  name={pie.name}
+                  price={pie.price}
+                  imageUrl={pie.imageUrl}
+                />
+                <div className="pie-info">
+                  <h4>{pie.name}</h4>
+                  <p>${pie.price}</p>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
--- app/shop/fruit/page.tsx
+++ app/shop/fruit/page.tsx
@@ -1,7 +1,45 @@
-export default function Fruit() {
+import Image from "next/image";
+import AddToCartButton from "@/ui/components/Button/AddToCartButton";
+
+export default async function Fruit() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/pies?category=fruit`
+  );
+  const pies = await res.json();
+
   return (
     <>
-      <h2>Shop for Fruit Pies</h2>
+      <section className="main-content">
+        <h2>
+          Fruit Pies
+          <hr />
+        </h2>
+
+        <div className="gallery-wrapper">
+          {pies.map((pie: any) => (
+            <>
+              <div key={pie.id} className="pie-item">
+                <Image
+                  src={pie.imageUrl}
+                  alt={pie.name}
+                  width={400}
+                  height={200}
+                />
+                <AddToCartButton
+                  id={pie.id}
+                  name={pie.name}
+                  price={pie.price}
+                  imageUrl={pie.imageUrl}
+                />
+                <div className="pie-info">
+                  <h4>{pie.name}</h4>
+                  <p>${pie.price}</p>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
